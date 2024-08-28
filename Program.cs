0c067b0441d9a56b8c78592b91d86c3cab9ba44a
esraa <esraa@DESKTOP-9RKGVE6>
2024-08-28 22:07:17 +0300

    Deffirent Between Map & MapGet & MapPost

--- Program.cs
+++ Program.cs
@@ -6,9 +6,22 @@ namespace MVC01
         {
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World sesstion one");
-
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/Home", async context =>
+                {
+                    await context.Response.WriteAsync("you are in home page");
+                });
+                endpoints.MapPost("/Products", async context =>
+                {
+                    await context.Response.WriteAsync("you are in products page");
+                });
+            });
+            app.Run(async (HttpContext) =>
+            {
+                await HttpContext.Response.WriteAsync("your requested page not found");
+            });
             app.Run();
         }
     }
