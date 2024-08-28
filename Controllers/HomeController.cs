86a7af2d6023388775e330cb9e8a22a7ae525b28
esraa <esraa@DESKTOP-9RKGVE6>
2024-08-28 22:58:51 +0300

    Action Result

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult  Index()
+        {
+
+            //ContentResult contentResult = new ContentResult();
+            //contentResult.Content = "hello from content result";
+            //return contentResult;
+            //return Content("hello from content");
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View(); 
+        }
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
